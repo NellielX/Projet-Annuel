b1cb5da0478d80f1b570efe61a5830042fba7c16
NellielX <gae.marzilli@gmail.com>
2014-08-31 15:48:11 +0200

    Bug taille boutons

--- EveryThing/src/vue/PanelOutil.java
+++ EveryThing/src/vue/PanelOutil.java
@@ -39,7 +39,7 @@ public class PanelOutil extends JPanel{
 //		vue_principale.New_vue_Dessin.panel_pour_outils.add(rectangle);
 //		vue_principale.New_vue_Dessin.panel_pour_outils.revalidate();
 		
-		int width = 200;
+		int width = 150;
 	    int height = 50;
 	    Dimension dim = new Dimension(width, height);
 	    
@@ -48,25 +48,12 @@ public class PanelOutil extends JPanel{
 		panel_outils.setBackground(SystemColor.menu);
 		GridBagLayout gbl_panel_outils = new GridBagLayout();
 		gbl_panel_outils.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_outils.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_outils.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_outils.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_outils.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_outils.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_outils.setLayout(gbl_panel_outils);
 		
-		//Jbutton rectangle
-		JButton btnOutilRectangle = new JButton("");
-		btnOutilRectangle.setPreferredSize(dim);	
-		btnOutilRectangle.setBackground(new Color(139, 69, 19));
-		btnOutilRectangle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Ceci est un beau rectangle !");
-			}
-		});
-		GridBagConstraints gbc_btnOutilRectangle = new GridBagConstraints();
-		gbc_btnOutilRectangle.insets = new Insets(0, 0, 5, 5);
-		gbc_btnOutilRectangle.gridx = 6;
-		gbc_btnOutilRectangle.gridy = 5;
-		panel_outils.add(btnOutilRectangle, gbc_btnOutilRectangle);
+		vue_principale.New_vue_Dessin.panel_pour_outils.add(panel_outils);
 		
 		//Jbutton ligne
 		JButton btnOutilLigne = new JButton("");
@@ -79,11 +66,24 @@ public class PanelOutil extends JPanel{
 		});
 		GridBagConstraints gbc_btnOutilLigne = new GridBagConstraints();
 		gbc_btnOutilLigne.insets = new Insets(0, 0, 5, 5);
-		gbc_btnOutilLigne.gridx = 6;
-		gbc_btnOutilLigne.gridy = 3;
+		gbc_btnOutilLigne.gridx = 5;
+		gbc_btnOutilLigne.gridy = 1;
 		panel_outils.add(btnOutilLigne, gbc_btnOutilLigne);
 		
-		vue_principale.New_vue_Dessin.panel_pour_outils.add(panel_outils);
+		//Jbutton rectangle
+		JButton btnOutilRectangle = new JButton("");
+		btnOutilRectangle.setPreferredSize(dim);	
+		btnOutilRectangle.setBackground(new Color(139, 69, 19));
+		btnOutilRectangle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Ceci est un beau rectangle !");
+			}
+		});
+		GridBagConstraints gbc_btnOutilRectangle = new GridBagConstraints();
+		gbc_btnOutilRectangle.insets = new Insets(0, 0, 5, 5);
+		gbc_btnOutilRectangle.gridx = 5;
+		gbc_btnOutilRectangle.gridy = 2;
+		panel_outils.add(btnOutilRectangle, gbc_btnOutilRectangle);
 		vue_principale.New_vue_Dessin.panel_pour_outils.revalidate();
 	}
 
